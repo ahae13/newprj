8a89184e101148a7f1f5a073131baa7ace5e2405
jsh <asin@naver.com>
2023-02-06 20:08:12 +0900

    1

---
+++ newprj/src/main/java/org/greenp/newprj/NewprjApplication.java
@@ -0,0 +1,13 @@
+package org.greenp.newprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprjApplication.class, args);
+	}
+
+}
---
+++ newprj/src/test/java/org/greenp/newprj/NewprjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greenp.newprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
